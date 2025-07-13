cf501d4d3992de5d77736a51bb441a32a6722f3b
Michele Grieco <michelegrieco92@gmail.com>
2025-07-13 13:37:31 +0200

    method to retrieve an engineer by id

--- src/main/java/com/example/hellospringboot/SoftwareEngineerController.java
+++ src/main/java/com/example/hellospringboot/SoftwareEngineerController.java
@@ -3,6 +3,7 @@ package com.example.hellospringboot;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,11 @@ public class SoftwareEngineerController {
      */
     public List<SoftwareEngineer> getEngineers() {
             return softwareEngineerService.getAllSoftwareEngineers();
+    }
+
+    @GetMapping("{id}")
+    public SoftwareEngineer getEngineerById(@PathVariable Integer id) {
+            return softwareEngineerService.getSoftwareEngineerById(id);
     }
 
     @PostMapping
