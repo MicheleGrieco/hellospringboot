01a497f85a58c432cb6181388282b16ed86d6d5c
Michele Grieco <michelegrieco92@gmail.com>
2025-07-14 14:40:15 +0200

    test templates

---
+++ src/test/java/com/example/hellospringboot/SoftwareEngineerServiceTests.java
@@ -0,0 +1,33 @@
+package com.example.hellospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SoftwareEngineerServiceTests {
+
+    @Test
+    public void getAllSoftwareEngineersTest() {
+        // This test will check if the context loads correctly for SoftwareEngineerService
+    }
+
+    @Test
+    public void insertSoftwareEngineerTest() {
+        // This test will check if a software engineer can be inserted correctly
+    }
+
+    @Test
+    public void getSoftwareEngineerByIdTest() {
+        // This test will check if a software engineer can be retrieved by ID
+    }
+
+    @Test
+    public void deleteSoftwareEngineerByIdTest() {
+        // This test will check if a software engineer can be deleted by ID
+    }
+
+    @Test
+    public void updateSoftwareEngineerTest() {
+        // This test will check if a software engineer can be updated correctly
+    }
+}
