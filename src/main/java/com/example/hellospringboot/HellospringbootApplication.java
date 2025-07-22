7165f37d0eb1d21b1386df37bc87d3914e4fe187
Michele Grieco <michelegrieco92@gmail.com>
2025-07-22 16:22:51 +0200

    dotenv integration

--- src/main/java/com/example/hellospringboot/HellospringbootApplication.java
+++ src/main/java/com/example/hellospringboot/HellospringbootApplication.java
@@ -1,15 +1,21 @@
 package com.example.hellospringboot;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @RestController
 public class HellospringbootApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach((entry) -> 
+			System.setProperty(entry.getKey(), entry.getValue())
+		);
 		SpringApplication.run(HellospringbootApplication.class, args);
 	}
 
